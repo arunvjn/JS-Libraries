dc332deb37b1d5483c49063c80799396d9d550ef
arunvjn <32433245+arunvjn@users.noreply.github.com>
2023-10-25 18:59:05 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,21 @@
+// Define your functions and variables
+function myFunction1() {
+  return 'function1!';
+}
+
+function myFunction2(str) {
+  return `You entered ${str}`;
+}
+
+const myVariable1 = 'Value 1';
+const myVariable2 = 42;
+
+// Export the functions and variables as a toolkit object
+const toolkit = {
+  myFunction1,
+  myFunction2,
+  myVariable1,
+  myVariable2,
+};
+
+export { myFunction1, myFunction2, myVariable1, myVariable2, toolkit };
